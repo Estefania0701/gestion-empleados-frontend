84727f9dacd5a0e82e49c6a3fc378c8ccbe11a44
Estefanía Aguas <estefaniaaguas@gmail.com>
2023-04-27 17:30:33 -0500

    Importo servicio para obtener lista de empleados

--- src/app/lista-empleados/lista-empleados.component.ts
+++ src/app/lista-empleados/lista-empleados.component.ts
@@ -1,5 +1,6 @@
+import { EmpleadoService } from '../empleado.service';
 import { Empleado } from './../empleado';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core'; //decoradores e interfaces de Angular necesarios para definir un componente
 
 
 /*
@@ -26,24 +27,62 @@ export class ListaEmpleadosComponent implements OnInit{
   // Propiedad, array de objetos Empleado
   empleados:Empleado[];
 
-  constructor() {}
+  /* Recibe una instancia de EmpleadoService como inyección de dependencia. 
+  Esto permite que el componente acceda al servicio para obtener los empleados.*/
+  constructor(private empleadoServicio : EmpleadoService) {}
 
   // se ejecuta cuando el componente se inicializa
   ngOnInit(): void {
-    /*En este caso, se proporcionan datos de ejemplo directamente en el código, 
-    pero en una aplicación real, estos datos podrían provenir de una API o una 
-    base de datos. */
-    this.empleados = [{
-      "id":1,
-      "nombre":"Estefanía",
-      "apellido":"Aguas",
-      "email":"estefaniaaguas@gmail.com"
-    },
-    {
-      "id":2,
-      "nombre":"Jassir",
-      "apellido":"Agressoth",
-      "email":"jassirbebe@gmail.com"
-    }];
+    this.obtenerEmpleados(); // obtengo la lista de empleados
+  }
+
+
+  /* Utiliza el servicio empleadoServicio para obtener la lista de empleados. 
+  Se suscribe al Observable devuelto por el método obtenerListaDeEmpleados() 
+  del servicio. Cuando se reciben los datos (empleados), se asignan a la 
+  propiedad empleados del componente.
+  VER ABAJO MÁS INFO SOBRE SUSCRIBIRSE */
+  private obtenerEmpleados() {
+    this.empleadoServicio.obtenerListaDeEmpleados().subscribe(dato => {
+      this.empleados = dato;
+    })
   }
 }
+
+
+/* 
+---------------------------------- SUSCRIBIRSE ------------------------------
+
+Cuando se trabaja con Observables en Angular, la acción de suscribirse significa 
+establecer una conexión entre el Observable y un observador (también conocido 
+  como suscriptor). El observador es un objeto que define cómo manejar los 
+  valores emitidos por el Observable.
+
+La suscripción se realiza llamando al método subscribe() del Observable y 
+proporcionando uno o varios callbacks que se ejecutarán cuando se emitan valores, 
+se produzcan errores o se complete el flujo de datos. Estos callbacks se definen 
+como funciones anónimas o funciones definidas previamente.
+
+El método subscribe() acepta hasta tres argumentos:
+  - next: Este argumento es una función que se ejecuta cuando se emite un nuevo 
+  valor en el Observable. El valor emitido se pasa como argumento a esta función. 
+  Es en este callback donde se manejan los valores recibidos y se realizan las 
+  acciones necesarias.
+  - error: Este argumento es una función que se ejecuta cuando se produce un 
+  error en el Observable. El error se pasa como argumento a esta función. Aquí 
+  se manejan los errores y se toman las medidas necesarias, como mostrar un 
+  mensaje de error o realizar alguna acción específica.
+  - complete: Este argumento es una función que se ejecuta cuando el Observable 
+  completa su flujo de datos, es decir, cuando no hay más valores que emitir. 
+  Este callback se utiliza para realizar acciones de limpieza o finalización, 
+  por ejemplo, deshabilitar un botón o cerrar una suscripción.
+
+Una vez que te has suscrito a un Observable, el observador permanece a la espera 
+de que se emitan valores y ejecuta los callbacks correspondientes de acuerdo a 
+los eventos ocurridos. La suscripción se mantiene activa hasta que se complete, 
+se cancele explícitamente o se produzca un error.
+
+Es importante destacar que es recomendable desuscribirse de un Observable 
+cuando ya no se necesita recibir más valores. Esto se hace llamando al método 
+unsubscribe() en el objeto devuelto por el método subscribe(). La desuscripción 
+libera recursos y evita posibles fugas de memoria.*/
