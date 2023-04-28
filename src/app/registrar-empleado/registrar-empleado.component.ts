81bfb791d29ec9bd77dff81b11d77f2ebac025ec
Estefanía Aguas <estefaniaaguas@gmail.com>
2023-04-28 11:35:07 -0500

    Defino guardarEmpleado() e irALaListaDeEmpleados()

--- src/app/registrar-empleado/registrar-empleado.component.ts
+++ src/app/registrar-empleado/registrar-empleado.component.ts
@@ -2,6 +2,8 @@
 
 import { Component, OnInit} from '@angular/core';
 import { Empleado } from '../empleado';
+import { EmpleadoService } from '../empleado.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-registrar-empleado',
@@ -13,10 +15,29 @@ export class RegistrarEmpleadoComponent implements OnInit{
   // creo propiedad empleado de tipo Empleado
   empleado : Empleado = new Empleado();
 
-  constructor() {}
+  /* Se inyectan los servicios EmpleadoService y Router en las propiedades 
+  empleadoServicio y router, respectivamente. */
+  constructor(private empleadoServicio : EmpleadoService, private router : Router) {}
 
-  ngOnInit(): void {
-      
+  ngOnInit(): void {    
+  }
+
+  /* Se ejecuta al hacer click en el botón "Guardar" del formulario
+  Llama al método registrarEmpleado del servicio EmpleadoService, pasándole el 
+  objeto empleado. Luego, se suscribe al resultado del servicio utilizando el 
+  método subscribe. Si la operación es exitosa, muestra el resultado en la consola 
+  y redirige al usuario a la lista de empleados. Si ocurre algún error, lo muestra
+  en la consola.*/
+  guardarEmpleado() : void {
+    this.empleadoServicio.registrarEmpleado(this.empleado).subscribe(dato => {
+      console.log(dato);
+      this.irALaListaDeEmpleados();
+    }, error => console.log(error));
+  }
+
+  // Redirige a la lsita de empleados después de guardar un empleado
+  irALaListaDeEmpleados() {
+    this.router.navigate(['/empleados'])
   }
 
   onSubmit() {
