d58330179521aed552a1a308365d17bb537a9aa7
Estefanía Aguas <estefaniaaguas@gmail.com>
2023-04-28 10:17:29 -0500

    Creo propiedad de tipo Empleado para el formulario

--- src/app/registrar-empleado/registrar-empleado.component.ts
+++ src/app/registrar-empleado/registrar-empleado.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+// Este código define el componente RegistrarEmpleadoComponent en Angular.
+
+import { Component, OnInit} from '@angular/core';
+import { Empleado } from '../empleado';
 
 @Component({
   selector: 'app-registrar-empleado',
   templateUrl: './registrar-empleado.component.html',
   styleUrls: ['./registrar-empleado.component.css']
 })
-export class RegistrarEmpleadoComponent {
+export class RegistrarEmpleadoComponent implements OnInit{
+
+  // creo propiedad empleado de tipo Empleado
+  empleado : Empleado = new Empleado();
+
+  constructor() {}
+
+  ngOnInit(): void {
+      
+  }
+
+  onSubmit() {
+    console.log(this.empleado);
+  }
 
 }
